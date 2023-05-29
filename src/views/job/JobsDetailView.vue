ee645a5fb38d0f5976a3cf201014fec4a1f43395
Khunn Satt <kset.dkk@gmail.com>
2023-05-29 10:22:53 +0630

    Fetch Api & JSON Server

--- src/views/job/JobsDetailView.vue
+++ src/views/job/JobsDetailView.vue
@@ -1,13 +1,31 @@
 <template>
-  <h2>Job Detail {{ id }}</h2>
+  <div v-if="job">
+    <h2>Job Title - {{ job.title }}</h2>
+    <p>Job Detail - {{ job.detail }}</p>
+  </div>
 </template>
 
 <script>
 export default {
-props:['id']
+  props: ['id'],
+  data() {
+    return {
+      job: null,
+    }
+  },
+  mounted() {
+    fetch("http://localhost:3000/jobs/" + this.id)
+      .then(((response) => {
+        return response.json();
+      }))
+      .then((data) => {
+        this.job = data
+      })
+      .catch((err) => {
+        console.log(err);
+      })
+  },
 }
 </script>
 
-<style>
-
-</style>
+<style></style>
--- src/views/job/JobsView.vue
+++ src/views/job/JobsView.vue
@@ -1,25 +1,33 @@
 <template>
   <h2>Jobs</h2>
   <div v-for="job in jobs" :key="job.id">
-   
-    <router-link :to="{name:'Job/Detail', params:{id:job.id}}"> <p>{{ job.title }}</p></router-link>
+
+    <router-link :to="{ name: 'Job/Detail', params: { id: job.id } }">
+      <p>{{ job.title }}</p>
+    </router-link>
   </div>
 </template>
 
 <script>
 export default {
- data(){
-    return{
-        jobs:[
-            {id:1, title:'Html'},
-            {id:2, title:'Laravel'},
-            {id:3, title:'Vue'},
-        ]
+  data() {
+    return {
+      jobs: []
     }
- }
+  },
+  mounted() {
+    fetch("http://localhost:3000/jobs")
+      .then(((response) => {
+        return response.json();
+      }))
+      .then((datas) => {
+        this.jobs = datas
+      })
+      .catch((err) => {
+        console.log(err);
+      })
+  },
 }
 </script>
 
-<style>
-
-</style>
+<style></style>
